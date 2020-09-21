1b9bf7f6f7c8a0f826b94b7610e58f993c87769c
Gauthier Rammault <gauthier.rammault@icloud.com>
2020-09-21 18:28:08 +0200

    Initial Commit

---
+++ AR Magic/AR_MagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_MagicApp.swift
+//  AR Magic
+//
+//  Created by Gauthier Rammault on 21/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct AR_MagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
